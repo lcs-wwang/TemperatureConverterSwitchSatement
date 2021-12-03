009c5e01b690fc55dbfe7ccf9cc782f2de2797e9
winstonwang <wwang@lcs.on.ca>
2021-12-03 11:13:56 -0500

    have the project ready to provide output to the user about weather

--- TemperatureConverterSwitchSatement/ContentView.swift
+++ TemperatureConverterSwitchSatement/ContentView.swift
@@ -1,21 +1,76 @@
 //
 //  ContentView.swift
-//  TemperatureConverterSwitchSatement
+//  TemperatureConverter
 //
-//  Created by Winston Wang on 2021-12-03.
+//  Created by Russell Gordon on 2021-11-30.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    @State var temperatureInCelsius = 0.0
+    
+    // MARK: Computed properties
+    var temperatureInFahrenheit: Double {
+        return temperatureInCelsius * 1.8 + 32
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+
+            HStack {
+                Text("Celsius")
+                    .bold()
+
+                Spacer()
+            }
+            
+            // Input: Temperature in Celsius
+            Slider(value: $temperatureInCelsius,
+                   in: -50.0...50.0,
+                   step: 0.1,
+                   label: {
+                Text("Celsius")
+            },
+                   minimumValueLabel: {
+                Text("-50")
+            },
+                   maximumValueLabel: {
+                Text("50")
+            })
+            
+            // Output: Temperature in Celsius
+            Text("\(String(format: "%.1f", temperatureInCelsius)) °C")
+                .bold()
+                .padding()
+            
+            HStack {
+                Text("Fahrenheit")
+                    .bold()
+                
+                Spacer()
+            }
+
+            // Output: Temperature in Fahrenheit
+            Text("\(String(format: "%.1f", temperatureInFahrenheit)) °F")
+                .bold()
+                .padding()
+            
+            Spacer()
+            
+        }
+        .padding()
+        .navigationTitle("Temperature Converter")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
