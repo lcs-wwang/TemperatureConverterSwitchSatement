4f0e765353fb9475327791e1c79fceb75a72c7e7
winstonwang <wwang@lcs.on.ca>
2021-12-03 11:46:19 -0500

    example of expressing the same logic as the if statement with  a swift statement instead both are examples of selection statement the syntax is all that's different

--- TemperatureConverterSwitchSatement/ContentView.swift
+++ TemperatureConverterSwitchSatement/ContentView.swift
@@ -19,18 +19,19 @@ struct ContentView: View {
     }
     // to provide suggestion of what to wear based on temp
     var feedback:String{
-        if temperatureInCelsius < -20.0 {
-            return "It's freezing outside! 🥶"
-        } else if temperatureInCelsius < 0.0 {
-            return "Dress warmly!"
-        } else if temperatureInCelsius < 10.0 {
-            return "Need a coat!"
-        } else if temperatureInCelsius < 20.0 {
-            return "Not very cold, not very hot... Perfect!"
-        } else if temperatureInCelsius < 30.0 {
-            return "Warm weather! Wear thin layers."
-        } else {
-            return "Never go out! It's too hot. 🥵"
+        switch temperatureInCelsius {
+            case ...(-20):
+                return "It's freezing outside! 🥶"
+            case -20...0:
+                return "Dress warmly!"
+            case 0...10:
+                return "Need a coat!"
+            case 10...20:
+                return "Not very cold, not very hot... Perfect!"
+            case 20...30:
+                return "Warm weather! Wear thin layers."
+            default:
+                return "Never go out! It's too hot. 🥵"
         }
     }
     //"body" property is only for input and output
