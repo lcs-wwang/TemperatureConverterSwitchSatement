24bc4c9fc53c9d048bf569df9a92cd11c94ef4ae
winstonwang <wwang@lcs.on.ca>
2021-12-03 11:10:00 -0500

    Initial Commit

---
+++ TemperatureConverterSwitchSatement/TemperatureConverterSwitchSatementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterSwitchSatementApp.swift
+//  TemperatureConverterSwitchSatement
+//
+//  Created by Winston Wang on 2021-12-03.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterSwitchSatementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
