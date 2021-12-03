643b42c9dcf5405cc73fce79c7494da3014bad2f
winstonwang <wwang@lcs.on.ca>
2021-12-03 11:18:51 -0500

    event that check two condition joined by a logical and operator to provide feedback

--- TemperatureConverterSwitchSatement/ContentView.swift
+++ TemperatureConverterSwitchSatement/ContentView.swift
@@ -59,8 +59,23 @@ struct ContentView: View {
                 .bold()
                 .padding()
             
-            Spacer()
+            //output: what to wear
+            if temperatureInCelsius >= -50.0 && temperatureInCelsius < -20.0 {
+                Text("It's freezing outside! 🥶")
+            } else if temperatureInCelsius >= -20.0 && temperatureInCelsius < 0.0 {
+                Text("Dress warmly!")
+            } else if temperatureInCelsius >= 0.0 && temperatureInCelsius < 10.0 {
+                Text("Need a coat!")
+            } else if temperatureInCelsius >= 10.0 && temperatureInCelsius < 20.0 {
+                Text("Not very cold, not very hot... Perfect!")
+            } else if temperatureInCelsius >= 20.0 && temperatureInCelsius < 30.0 {
+                Text("Warm weather! Wear thin layers.")
+            } else if temperatureInCelsius >= 30.0 && temperatureInCelsius <= 50.0 {
+                Text("Never go out! It's too hot. 🥵")
+            }
             
+            Spacer()
+
         }
         .padding()
         .navigationTitle("Temperature Converter")
